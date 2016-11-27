18271e163861397ba98174a892204d59de6702d5
Dominik <dominik.goralczyk@gmail.com>
2016-11-27 02:20:55 +0100

    linear regression with 1 variable and plotting added

--- 03 regression/regression.py
+++ 03 regression/regression.py
@@ -5,13 +5,13 @@ import sklearn.model_selection as modsel
 import matplotlib.pyplot as plt
 
 
-
 def cost(theta, X, Y):
     m = X[:,1].size
     cost = np.dot(X, theta) - Y
     cost **= 2
     cost = cost.sum() / 2 / m
     return cost
+
 
 def gradient_step(theta, X, Y, alpha):
     m = X[:,1].size
@@ -22,6 +22,7 @@ def gradient_step(theta, X, Y, alpha):
     new_theta = theta - sub
     return new_theta
 
+
 def gradient_descent(theta, x, y, alpha, steps):
     for i in range(steps):
         if i%100 == 0:
@@ -29,43 +30,41 @@ def gradient_descent(theta, x, y, alpha, steps):
         theta = gradient_step(theta, x, y, alpha)
     return theta
 
+def y_plotLine(x, theta):
+    return theta[0] + x*theta[1]
+
 
 #load and format data
 data = np.genfromtxt("data.csv", delimiter='\t')
 X = data[:,0]
 Y = data[:,1]
-
-N = len(X)
+m = len(X)
 Y = np.array(Y)
-Y = Y.reshape((N, 1))
-
+Y = Y.reshape((m, 1))
 X = np.array(X)
-X = X.reshape((N, 1))
-
-"""
-show data
-plt.plot(X, Y, 'r.')
-plt.show()
-"""
-
+X = X.reshape((m, 1))
 #add ones column on the left
-ones1 = np.ones(N)[:, None]
-X = np.append(ones1, X, axis=1)    #Nx2 matrix
+ones1 = np.ones((m, 1))
+X = np.append(ones1, X, axis=1)    #m x (n+1) (80x2)  matrix
 
-initial_theta = np.zeros(2)[:, None]    #2x1 vector [b;a]
 
+### regression with one parameter y = ax + b
+
+initial_theta = np.zeros((2, 1))    #nx1 vector
 #prepare train and test sets
 X_train, X_test, Y_train, Y_test = modsel.train_test_split\
     (X, Y, test_size=0.33, random_state=42)
 
-#learning
 alpha = 1e-05
-theta = gradient_descent(initial_theta, X_train, Y_train, alpha, 2000)
+steps = 2000
+theta = gradient_descent(initial_theta, X_train, Y_train, alpha, steps)
 
 print("Training cost: ", cost(theta, X_train, Y_train))
 print("Test cost: ", cost(theta, X_test, Y_test))
 
-print(X_test)
-print(X_test[:,1])
-print(np.amin(X_test[:,1]))
-print(np.amax(X_test[:,1]))
+#plot data
+plt.plot(X_test[:,1], Y_test, 'r.')
+plt.plot(X_test[:,1], y_plotLine(X_test[:,1], theta))
+plt.show()
+
+
